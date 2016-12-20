4d1f5a75b15ba6aa9038dbc170b5929920b245d3
RoyTheunissen <roy.theunissen@live.nl>
2016-12-20 20:57:57 +0100

    The watermark now displays information as read from the build information file.

--- Scripts/UI/Watermark.cs
+++ Scripts/UI/Watermark.cs
@@ -1,35 +1,26 @@
-using RoyTheunissen.PlaytestRecording.Utilities;
+using RoyTheunissen.PlaytestRecording.Building;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace RoyTheunissen.PlaytestRecording.UI
 {
     /// <summary>
-    /// Manages watermarks to help you identify gameplay footage.
+    /// Displays a watermark based on the current build information to help you identify footage.
     /// </summary>
     public sealed class Watermark : MonoBehaviour 
     {
-        private const int ShaLength = 12;
-
-        [SerializeField]
-        private string addressee;
-
         [Space]
         [SerializeField]
         private Text text;
-
-        [SerializeField]
-        private PlaytestService playtestService;
 
         private void Awake()
         {
             string format = text.text;
 
-            string applicationName = playtestService.ApplicationName;
-            string versionName = playtestService.ApplicationVersion;
-            string sha = GitUtilities.Sha.Substring(0, ShaLength);
+            BuildInformation info = BuildInformation.Current;
 
-            text.text = string.Format(format, addressee, applicationName, versionName, sha);
+            text.text = string.Format(format,
+                info.Addressee, info.ApplicationName, info.VersionName, info.Sha);
         }
     }
 }
